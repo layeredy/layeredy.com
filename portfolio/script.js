0cb02fac02106180addc395a23afa63a590216d5
herberto <80128253+Cartyoo@users.noreply.github.com>
2024-10-07 15:30:33 -0700

    Remove Text Animation (#1)
    * Update index.html
    
    * Update script.js
    
    * Update styles.css

--- portfolio/script.js
+++ portfolio/script.js
@@ -19,33 +19,3 @@ function updateIcon(theme) {
         ? '<i class="bi bi-sun-fill"></i>' 
         : '<i class="bi bi-moon-fill"></i>';
 }
-
-// Typing effect
-document.addEventListener('DOMContentLoaded', function () {
-    const element = document.querySelector('.text-lowercase');
-    const words = ["business", "community", "project", "website", "app", "utility", "API", "concept", "solution", "resource", "tool", "vision", "idea"];
-    let index = 0;
-    let charIndex = 0;
-    const typingSpeed = 150;
-    let isDeleting = false;
-
-    function typeWriter() {
-        if (charIndex < words[index].length && !isDeleting) {
-            element.textContent += words[index].charAt(charIndex);
-            charIndex++;
-            setTimeout(typeWriter, typingSpeed);
-        } else if (charIndex > 0 && isDeleting) {
-            element.textContent = element.textContent.slice(0, -1);
-            charIndex--;
-            setTimeout(typeWriter, typingSpeed);
-        } else {
-            isDeleting = !isDeleting;
-            if (!isDeleting) {
-                index = (index + 1) % words.length;
-            }
-            setTimeout(typeWriter, 500);
-        }
-    }
-
-    typeWriter();
-});
